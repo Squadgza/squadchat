db7492b3f9229b79c2d161749b72c8fe059f1d9e
zachee1 <zachee.ishimwe@amalitech.org>
2023-11-17 08:35:44 +0200

    created relations between user, message and Rooms

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
@@ -8,6 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(process.cwd(), '/src/index.html'));
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
